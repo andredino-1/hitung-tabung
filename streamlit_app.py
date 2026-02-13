1eaf9175c78a5be27fcc1d22f341ccce2903d04f
andredino-1 <lisaagustinaa07@gmail.com>
2026-02-13 10:44:54 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,11 @@
 import streamlit as st
-st.title(TUTORIAL JADI BOS MUDA :blue[ANDRE DAN DINO] :money:")
+import math
+st.title("Menghitung :volume Tabung buatan andre&Dino[cool] :rocket:")
 
+
+r = st.number_input("Masukan Jari_Jari (cm): ",0)
+t = st.number_input("Masukan Tinggi (cm): ",0)
+
+if st.button("Hitung Volume", type="primary"):
+  v = math.pi*(r**2)*t
+  St.success(f'Volume Tabung adalah {v:2f},)
